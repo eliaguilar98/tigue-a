952ca8025dceea9da182636da04ccc7d60a1c244
Elizabeth <elizabeth.enriquez@seditec.mx>
2022-10-03 18:32:36 +0000

    [CHANGE FILD VALUE TYPE]

--- customdate/models/models.py
+++ customdate/models/models.py
@@ -3,8 +3,7 @@
 from odoo import models, fields, api
 
 
- class customdate(models.Model):
-     
+class customdate(models.Model):
      _inherit = 'project.task.recurrence'
     
      next_recurrence_date = fields.Datetime()
